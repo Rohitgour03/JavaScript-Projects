be1e22d81e8861d06d8c9d1f392a52d20b01d126
Rohit Gour <rohitgourrg999@gmail.com>
2022-03-29 18:42:09 +0530

    Added desktop sized layouts and modal

--- E-Commerce product page/main.js
+++ E-Commerce product page/main.js
@@ -37,8 +37,8 @@
 // Function for showing the Product Slideshow at mobile size
 (function productSlideshow() {
 
-    const prevBtn = document.querySelector('.prev');
-    const nextBtn = document.querySelector('.next');
+    const prevBtn = document.querySelectorAll('.prev');
+    const nextBtn = document.querySelectorAll('.next');
 
     let slideIndex = 1;
     showSlides(slideIndex);
@@ -59,12 +59,76 @@
         }
 
         slides[slideIndex - 1].style.display = "block";
+
     }
 
-    prevBtn.addEventListener('click', () => {
-        plusSlides(-1)
+    prevBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            plusSlides(-1)
+        })
     })
-    nextBtn.addEventListener('click', () => {
-        plusSlides(1)
+    nextBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            plusSlides(1)
+        })
     })
 })();
+
+// Function for slideshow of modal product showcase
+(function productSlideshowDesktop() {
+
+    const prevBtn = document.querySelectorAll('.modal__prev');
+    const nextBtn = document.querySelectorAll('.modal__next');
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides__desktop");
+
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+
+    }
+
+    prevBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            plusSlides(-1)
+        })
+    })
+    nextBtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            plusSlides(1)
+        })
+    })
+})()
+
+function closePdtSlideshowModal() {
+    const mainPdtCtn = document.querySelector('.main-pdt-ctn');
+    const closeBtn = document.querySelector('.pdt-showcase-model__close-btn-ctn')
+    const modal = document.querySelector('.pdt-showcase-modal__desktop')
+    const overlay = document.querySelector('.overlay-nav')
+
+    mainPdtCtn.addEventListener('click', () => {
+        modal.classList.add('show-pdt-slideshow-modal')
+        overlay.classList.add('show-overlay')
+    })
+
+    closeBtn.addEventListener('click', (event) => {
+        modal.classList.remove('show-pdt-slideshow-modal')
+        overlay.classList.remove('show-overlay')
+    })
+
+}
+closePdtSlideshowModal();
